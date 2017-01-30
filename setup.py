f6bec87fdde3296e36263e1f6aee8c5ac84e6d0a
Bruce Fenske <bwfenske@ualberta.ca>
2017-01-30 14:42:04 -0700

    pypi changes to reflect python 3 support

--- setup.py
+++ setup.py
@@ -29,7 +29,13 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7 :: 3',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ]
 
