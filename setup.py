82aedca6f5e9e38aaf8d8e48a3a53f247f6a1840
scls19fr <s.celles@gmail.com>
2017-07-01 22:50:35 +0200

    Remove broken versions

--- setup.py
+++ setup.py
@@ -31,11 +31,10 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.1',
-        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ]
 
