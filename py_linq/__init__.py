92bfa03aa03cab64e0b71aea561dc9142dd284ef
Bruce Fenske <bwfenske@ualberta.ca>
2017-07-13 15:39:03 -0600

    PEP8 coding style on main branch completed. Use noqa for unused import PEP8 error

--- py_linq/__init__.py
+++ py_linq/__init__.py
@@ -1,6 +1,6 @@
 __version__ = '0.4.0'
 
 try:
-    from py_linq import Enumerable
+    from py_linq import Enumerable  # noqa
 except ImportError:
-    from py_linq.py_linq import Enumerable3 as Enumerable
+    from py_linq.py_linq import Enumerable3 as Enumerable  # noqa
