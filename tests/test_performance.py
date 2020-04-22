1576c06c480bd10f3d8870360252c07738afcef6
Bruce Fenske <bwfenske@ualberta.ca>
2020-04-21 22:43:35 -0600

    Replaced perf_counter function call with clock function call to support performance tests on py2.7

--- tests/test_performance.py
+++ tests/test_performance.py
@@ -11,34 +11,34 @@ class GeneratorPerformanceTests(unittest.TestCase):
         self.logger = logging.getLogger(__name__)
 
     def test_constructor(self):
-        tic = time.perf_counter()
+        tic = time.clock()
         for i in range(0, self.num_experiments):
             e = Enumerable(self.test_generator)
-        toc = time.perf_counter()
+        toc = time.clock()
         py_linq_time = (toc - tic) / self.num_experiments
         self.assertTrue(py_linq_time < 0.035)
 
     def test_select(self):
-        tic = time.perf_counter()
+        tic = time.clock()
         for i in range(0, self.num_experiments):
             e = Enumerable(self.test_generator).select(lambda b: {"id": b["id"]})
-        toc = time.perf_counter()
+        toc = time.clock()
         py_linq_time = (toc - tic) / self.num_experiments
         self.assertTrue(py_linq_time < 0.0035)
 
     def test_to_list(self):
         num_experiments = 10
-        tic = time.perf_counter()
+        tic = time.clock()
         for i in range(0, num_experiments):
             e = (
                 Enumerable(({"id": x, "value": x} for x in range(0, 80000)))
                 .select(lambda b: {"id": b["id"]})
                 .to_list()
             )
-        toc = time.perf_counter()
+        toc = time.clock()
         py_linq_time = (toc - tic) / num_experiments
 
-        tic = time.perf_counter()
+        tic = time.clock()
         for i in range(0, num_experiments):
             l = list(
                 map(
@@ -46,10 +46,10 @@ class GeneratorPerformanceTests(unittest.TestCase):
                     ({"id": x, "value": x} for x in range(0, 80000)),
                 )
             )
-        toc = time.perf_counter()
+        toc = time.clock()
         python_list_time = (toc - tic) / num_experiments
 
-        tic = time.perf_counter()
+        tic = time.clock()
         for i in range(0, num_experiments):
             l = [
                 i
@@ -58,7 +58,7 @@ class GeneratorPerformanceTests(unittest.TestCase):
                     ({"id": x, "value": x} for x in range(0, 80000)),
                 )
             ]
-        toc = time.perf_counter()
+        toc = time.clock()
         python_list_comp_time = (toc - tic) / num_experiments
 
         self.assertTrue(py_linq_time < python_list_comp_time * 10)
