9443b64d0db072cf148ab7926279e25fe988d8d9
NigelBess <nigelbess@gmail.com>
2024-04-16 09:36:45 -0700

    fixed bug in Enumerable.Any() that failed to account for 'None' values. Updated unit tests to match.

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -13,6 +13,7 @@ import six
 import pytest
 from tests.fixtures import (
     _simple,
+    _simpleWithNone,
     _complex,
     _locations,
     simple_generator,
@@ -173,6 +174,7 @@ def test_element_at_error(enumerable: Callable, index: int, error: Exception) ->
         (Enumerable(_complex).any(), True),
         (Enumerable(_complex).any(lambda x: x["value"] < 1), False),
         (Enumerable(_complex).any(lambda x: x["value"] > 1), True),
+        (Enumerable(_simpleWithNone).any(lambda x: x==None), True),
         (Enumerable().contains(1), False),
         (Enumerable(_simple).contains(1), True),
         (Enumerable(_complex).select(lambda x: x["value"]).contains(1), True),
